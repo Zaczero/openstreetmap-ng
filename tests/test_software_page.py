6e4fe01bf8a2b1f7ae69929de9d87cf0ca053578
tngus6007 <32013504+tngus6007@users.noreply.github.com>
2026-09-10 10:35:36 +0900

    Accept minified HTML form attributes in directory test

--- tests/test_software_page.py
+++ tests/test_software_page.py
@@ -3,7 +3,7 @@ async def test_software_directory(client):
     assert response.status_code == 200
     assert 'Organic Maps' in response.text
     assert 'StreetComplete' not in response.text
-    assert 'method="GET"' in response.text
+    assert 'method=GET' in response.text or 'method="GET"' in response.text
 
 
 async def test_software_directory_unknown_filter(client):
